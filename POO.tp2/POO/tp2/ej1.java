0c3fcb7d663d7715b80399124b24a2aa06b6ecb7
AgusB32 <fr.abenedetti1@gmail.com>
2024-04-24 22:21:34 -0300

    POO_TP2

---
+++ POO.tp2/POO/tp2/ej1.java
@@ -0,0 +1,7 @@
+package POO.tp2;
+
+public class ej1 {
+public static void main(String[] args) {
+    System.out.println("Hola mundo");
+}
+}
---
+++ POO.tp2/POO/tp2/ej4.java
@@ -0,0 +1,41 @@
+package POO.tp2;
+
+import java.util.Arrays;
+
+public class ej4 {
+        public static void main(String[] args) {
+            int[] numeros = {4, 2, 3, 8, 1};
+    
+            imprimirInformacion(numeros);
+        }
+    
+        public static void imprimirInformacion(int[] array) {
+            System.out.println("Cantidad de números el array: " + array.length);
+    
+            int mayor = array[0];
+            for (int num : array) {
+                if (num > mayor) {
+                    mayor = num;
+                }
+            }
+            System.out.println("El mayor número en el array: " + mayor);
+    
+            double suma = 0;
+            for (int num : array) {
+                suma += num;
+            }
+            double promedio = suma / array.length;
+            System.out.println("El promedio de los números en el array: " + promedio);
+    
+            Arrays.sort(array);
+            System.out.println("Números ordenados de menor a mayor: " + Arrays.toString(array));
+    
+            int[] arrayInverso = Arrays.copyOf(array, array.length);
+            for (int i = 0; i < arrayInverso.length / 2; i++) {
+                int temp = arrayInverso[i];
+                arrayInverso[i] = arrayInverso[arrayInverso.length - 1 - i];
+                arrayInverso[arrayInverso.length - 1 - i] = temp;
+            }
+            System.out.println("Números ordenados de mayor a menor: " + Arrays.toString(arrayInverso));
+        }
+    }

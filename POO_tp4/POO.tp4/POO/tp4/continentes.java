820867b4d343b056adbc698ab5a689f258ce5894
AgusB32 <fr.abenedetti1@gmail.com>
2024-04-26 21:17:48 -0300

    continentes_tp4

---
+++ POO_tp4/POO.tp4/POO/tp4/continentes.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class continentes {
+        private String nombre;
+        private List<Pais> paises;
+    
+        public Continente(String nombre) {
+            this.nombre = nombre;
+            this.paises = new ArrayList<>();
+        }
+    
+        public String getNombre() {
+            return nombre;
+        }
+    
+        public void agregarPais(Pais pais) {
+            paises.add(pais);
+        }
+    
+        public List<Pais> getPaises() {
+            return paises;
+        }
+    
+        @Override
+        public String toString() {
+            return "Continente: " + nombre;
+        }
+    }
